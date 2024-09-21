d1d19bc11e9127a9cab9c25ff4d31cc7422bb1db
kiD-9 <d.kirill.ekb@gmail.com>
2024-09-21 14:07:29 +0500

    added registration models

---
+++ backend/WebApi/Models/EducationalOrganizationRegistrationInfoDto.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Models;
+
+public class EducationalOrganizationRegistrationInfoDto
+{
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
