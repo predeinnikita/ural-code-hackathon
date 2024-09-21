de39b2ffeaa20bad0ef2b7566c17ad88949f4709
Artem Kislov <artemkislov057@gmail.com>
2024-09-21 21:10:07 +0500

    fix

---
+++ backend/Domain/MapsterConfigs/CreateVacancyRequestConfig.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using Domain.Models.Vacancy;
+using Mapster;
+
+namespace Domain.MapsterConfigs;
+
+public class CreateVacancyRequestConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config
+            .NewConfig<CreateVacancyRequest, Vacancy>()
+            .Ignore(x => x.Id)
+            .Ignore(x => x.BusinessOrganization);
+    }
+}
