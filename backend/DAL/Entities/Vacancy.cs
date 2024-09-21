cbc25803d804124222bb12692a7051b597420127
Artem Kislov <artemkislov057@gmail.com>
2024-09-21 19:32:24 +0500

    добавил описание

--- backend/DAL/Entities/Vacancy.cs
+++ backend/DAL/Entities/Vacancy.cs
@@ -7,6 +7,7 @@ public sealed class Vacancy
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string? Description { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public decimal? Salary { get; set; }
--- backend/WebApi/Dto/Vacancy/VacancyDto.cs
+++ backend/WebApi/Dto/Vacancy/VacancyDto.cs
@@ -6,6 +6,7 @@ public class VacancyDto
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public string? Description { get; set; }
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
     public decimal? Salary { get; set; }
