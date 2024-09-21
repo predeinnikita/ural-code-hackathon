8413b4bbe33345df4b2a88cbaeb2afb56ed553df
DIMASIK <dimasik_burdin@inbox.ru>
2024-09-21 18:13:38 +0500

    aoiaoaoaoao

--- client/src/pages/create-vacancy/CreateVacancy.tsx
+++ client/src/pages/create-vacancy/CreateVacancy.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 import styles from "./CreateVacancy.module.scss";
 import { useFormik } from "formik";
-import { Button, Input } from "@mui/material";
+import { Button, Input, TextField } from "@mui/material";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import dayjs, { Dayjs } from "dayjs";
 
 interface CreateVacancyPayload {
   period: {
-    start: string;
-    end: string;
+    start: string | null;
+    end: string | null;
   };
   paymemt: string;
   workingHours: string;
@@ -30,22 +31,50 @@ export const CreateVacancy: FC = () => {
       } as const,
       onSubmit: () => {},
     });
+
+  console.log(values);
+
   return (
     <div className={styles.main}>
       <div className={styles.title}>Создание вакансии</div>
       <div className={styles.fields}>
-        {Object.entries(values).map(([key, value]) => {
+        {Object.entries(values).map(([key, value], index) => {
           if (key === "period") {
-            return <></>;
+            return null;
           }
 
-          return <Input key={key} value={value} name="key" />;
+          return (
+            <TextField
+              placeholder={key}
+              key={key + index}
+              value={value}
+              name={key}
+              variant="outlined"
+              onChange={handleChange}
+            />
+          );
         })}
-        <LocalizationProvider>
-          <DatePicker></DatePicker>
-        </LocalizationProvider>
+        <div className={styles.date}>
+          <DatePicker
+            name="period.start"
+            onChange={(e) => (
+              console.log(e),
+              handleChange({
+                target: { name: "period.start", values: e?.toISOString() },
+              })
+            )}
+          />
+          <DatePicker
+            name="period.end"
+            onChange={(e) =>
+              handleChange({
+                target: { name: "period.end", values: e?.toISOString() },
+              })
+            }
+          />
+        </div>
       </div>
-      <Button disabled={!isValid} onClick={submitForm}>
+      <Button disabled={!isValid} onClick={submitForm} variant="contained">
         Создать
       </Button>
     </div>
