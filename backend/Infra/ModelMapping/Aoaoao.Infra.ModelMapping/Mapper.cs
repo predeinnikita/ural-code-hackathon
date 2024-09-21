3e5393ee4993dfccb6a3f437de20e32a38e2c7ba
Artem Kislov <artemkislov057@gmail.com>
2024-09-21 16:48:25 +0500

    temp

---
+++ backend/Infra/ModelMapping/Aoaoao.Infra.ModelMapping/Mapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Mapster;
+
+namespace Aoaoao.Infra.ModelMapping;
+
+public static class Mapper
+{
+    public static TDestination Map<TDestination>(this object? source) =>
+        source!.Adapt<TDestination>();
+
+    public static void Map<TSource, TDestination>(this TSource? source, TDestination result) =>
+        source!.Adapt<TSource, TDestination>(result);
+
+    public static IQueryable<TDestination> MapQueryable<TDestination>(
+        this IQueryable source) =>
+        source.ProjectToType<TDestination>();
+
+    /// <summary>
+    /// Сканирование сборок и компиляция конфигураций
+    /// </summary>
+    public static void ScanAndEnsureConfigurations(Assembly rootAssembly)
+    {
+        var assemblies = new HashSet<Assembly>();
+        FillWithAssembliesWithPrefix(rootAssembly, "Aoaoao", assemblies);
+        TypeAdapterConfig.GlobalSettings.RequireDestinationMemberSource = true;
+        TypeAdapterConfig.GlobalSettings.Scan(assemblies.ToArray());
+        TypeAdapterConfig.GlobalSettings.Compile();
+    }
+
+    private static void FillWithAssembliesWithPrefix(Assembly rootAssembly, string prefix, ISet<Assembly> assemblies)
+    {
+        assemblies.Add(rootAssembly);
+
+        var assembliesWithPrefix = rootAssembly
+            .GetReferencedAssemblies()
+            .Where(x => x.Name!.Contains(prefix))
+            .Select(Assembly.Load)
+            .ToArray();
+
+        if (assembliesWithPrefix.Length == 0)
+            return;
+
+        foreach (var assembly in assembliesWithPrefix)
+        {
+            assemblies.Add(assembly);
+            if (!assemblies.Contains(assembly))
+                FillWithAssembliesWithPrefix(assembly, prefix, assemblies);
+        }
+    }
+}
