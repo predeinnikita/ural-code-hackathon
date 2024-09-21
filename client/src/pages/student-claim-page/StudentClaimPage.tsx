aeb8fd70c916106cd62686c4058d9e2830add147
npredein <npredein@profitbase.ru>
2024-09-21 21:52:27 +0500

    six

--- client/src/pages/student-claim-page/StudentClaimPage.tsx
+++ client/src/pages/student-claim-page/StudentClaimPage.tsx
@@ -10,20 +10,12 @@ import { StudentClaimListItemProps } from "../student-claim-list-page/components
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import { CustomTextarea } from "../../components/custom-textarea/CustomTextarea";
-<<<<<<< Updated upstream
 import { ROUTES } from "../../components/routing/routes";
 import { useNavigate } from "react-router-dom";
 
 export const StudentClaimPage: FC = () => {
-  const [tags, setTags] = useState<string[]>(["Тэг1", "Тэг2"]);
-=======
-import { useNavigate } from "react-router-dom";
-import { ROUTES } from "../../components/routing/routes";
-
-export const StudentClaimPage: FC = () => {
-  const [tags, setTags] = useState<any[]>(["Тэг1", "Тэг2"]);
+  const [tags, setTags] = useState<string[]>(["Frontend", "1 год опыта"]);
   const navigate = useNavigate();
->>>>>>> Stashed changes
 
   return (
     <Stack direction="column">
@@ -46,12 +38,11 @@ export const StudentClaimPage: FC = () => {
                 })}
               </Stack>
               <Grid sx={{ mt: "16px" }}>
-                <CustomTextarea minRows={3} placeholder={"Описание..."} />
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos culpa ex maiores id in. Illum libero quia facere nam consectetur? Nostrum doloribus autem delectus! Quidem ut illum accusamus deleniti mollitia!
               </Grid>
             </Stack>
-<<<<<<< Updated upstream
           </CardContent>
-          <CardActions>
+          <CardActions onClick={() => navigate(ROUTES.ORGANIZATION_CLAIM_LIST_PAGE)}>
             <Button size="small" variant="contained" color="primary">
               Принять
             </Button>
@@ -62,22 +53,5 @@ export const StudentClaimPage: FC = () => {
         </Card>
       </Grid>
     </Stack>
-=======
-            <Grid sx={{ mt: "16px" }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit aperiam possimus qui libero dignissimos eum soluta optio, at quos nihil officia in saepe ut a, ducimus cum natus ipsum error.
-            </Grid>
-          </Stack>
-        </CardContent>
-        <CardActions onClick={() => navigate(ROUTES.ORGANIZATION_CLAIM_LIST_PAGE)}>
-          <Button size="small" variant="contained" color="primary">
-            Принять
-          </Button>
-          <Button size="small" variant="outlined" color="primary">
-            Отказаться
-          </Button>
-        </CardActions>
-      </Card>
-    </Grid>
->>>>>>> Stashed changes
   );
 };
